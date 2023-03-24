5778ca7b05e7f5e5aaff30d02be5960666c6edc7
Michal Kotlicki <michal.kotlicki@pwc.com>
2023-03-24 09:36:21 +0100

    Refactor

--- dic.ts
+++ dic.ts
@@ -13,24 +13,22 @@ let shiftToNext = 0
 let previousColumnsAmount = 0
 
 data.split("\n").forEach((line) => {
-  const numbers = line.replace(/\-/g, " -").trim().split(/\s+/)
+  const rowNumbers = line.replace(/\-/g, " -").trim().split(/\s+/)
 
-  if(numbers.length <= 1) {
+  if(rowNumbers.length <= 1) {
     shiftToNext += previousColumnsAmount 
     return
   }
 
-  previousColumnsAmount = numbers.length + 1
+  previousColumnsAmount = rowNumbers.length
     
-  numbers.forEach((n, i) => {
+  rowNumbers.forEach((n, i) => {
     const satellite = res[i + shiftToNext] 
     if(!satellite) res[i + shiftToNext] = []
     if(Number.isNaN(n)) return
     res[i + shiftToNext]!.push(+n)
   })  
 }) 
-
-res = res.filter(x => x)
 
 const dic = new Map<number, number[]>()
 
